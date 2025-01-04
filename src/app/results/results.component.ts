feb910597cb5b7ee9117f79db21c46adc3abea5b
moussa500 <mohamed.moussa.0720@gmail.com>
2025-01-04 19:45:39 +0100

    first commit

---
+++ src/app/results/results.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Answers, Question } from '../quiz.model';
+
+
+@Component({
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+  styleUrls: ['./results.component.css']
+})
+export class ResultsComponent {
+  @Input() answers: Answers;
+  @Input() questions: Question[];
+}
